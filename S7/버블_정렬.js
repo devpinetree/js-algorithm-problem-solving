6ee3e41d40575da8f15cb9ca67ef85c9dc93d9f2
devpinetree <bosollee@gmail.com>
2021-08-07 21:56:50 +0900

    solved: Section 7-2 풀이

---
+++ S7/버블_정렬.js
@@ -0,0 +1,17 @@
+/**
+ * 오름차순 버블 정렬
+ */
+
+function solution(arr) {
+  let answer = arr;
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
+  return answer;
+}
+
+solution([13, 5, 11, 7, 23, 15]);
