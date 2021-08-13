017b96a1e84495709a1e90a035df149052e951eb
devpinetree <bosollee@gmail.com>
2021-08-13 15:12:06 +0900

    solved: Section 9 - 7 풀이

---
+++ S9/섬나라_아일랜드(BFS).js
@@ -0,0 +1,53 @@
+/**
+ * N*N의 섬나라 아일랜드의 지도가 격자판의 정보로 주어집니다.
+ * 각 섬은 1로 표시되어 상하좌우와 대각선으로 연결되어 있으며, 0은 바다입니다.
+ * 섬나라 아일랜드에 몇 개의 섬이 있는지 출력하세요.
+ * 영역 확인 문제이므로 queue로 BFS로도 풀이 가능
+ */
+
+function solution(arr) {
+  let answer = 0;
+  let board = arr.slice();
+  let n = board.length;
+  let dx = [-1, -1, 0, 1, 1, 1, 0, -1];
+  let dy = [0, 1, 1, 1, 0, -1, -1, -1];
+  let queue = [];
+
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      if (board[i][j] === 1) {
+        // console.log(i, j);
+        board[i][j] = 0;
+        queue.push([i, j]);
+        answer++;
+
+        while (queue.length) {
+          let [x, y] = queue.shift();
+          // console.log(x, y);
+          for (let k = 0; k < 8; k++) {
+            let nx = x + dx[k];
+            let ny = y + dy[k];
+            if (nx >= 0 && nx < n && ny >= 0 && ny < n && board[nx][ny] === 1) {
+              board[nx][ny] = 0;
+              queue.push([nx, ny]);
+            }
+          }
+        }
+        // console.log('BFS end');
+      }
+    }
+  }
+
+  // console.log(answer);
+  return answer;
+}
+
+solution([
+  [1, 1, 0, 0, 0, 1, 0],
+  [0, 1, 1, 0, 1, 1, 0],
+  [0, 1, 0, 0, 0, 0, 0],
+  [0, 0, 0, 1, 0, 1, 1],
+  [1, 1, 0, 1, 1, 0, 0],
+  [1, 0, 0, 0, 1, 0, 0],
+  [1, 0, 1, 0, 1, 0, 0],
+]); // 5
